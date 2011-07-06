4067957d98065baf84dffd2a5fdedc4243d6d4a9
James Herdman <james.herdman@me.com>
2011-07-06 14:18:18 -0400

    Whitespace police

--- lib/raemon/server.rb
+++ lib/raemon/server.rb
@@ -2,15 +2,15 @@ RAEMON_ENV = (ENV['RAEMON_ENV'] || 'development').dup unless defined?(RAEMON_ENV
 
 module Raemon
   module Server
-    
+
     class << self
       attr_accessor :config
-      
+
       def run
         @config = Configuration.new if config.nil?
         yield config if block_given?
       end
-      
+
       def startup!
         initialize_application
 
@@ -22,9 +22,9 @@ module Raemon
 
         # Start the master daemon
         config.logger.info "=> Booting #{server_name} (#{RAEMON_ENV})"
-        
+
         worker_klass = instance_eval(config.worker_klass)
-        
+
         Raemon::Master.start config.num_workers, worker_klass, {
           :name         => server_name,
           :pid_file     => pid_file,
@@ -34,23 +34,23 @@ module Raemon
           :memory_limit => config.memory_limit
         }
       end
-      
+
       def shutdown!
         Raemon::Master.stop :pid_file => pid_file
       end
-      
+
       def console!
         initialize_application
       end
-      
+
       def initialize_application
         load_environment
         load_initializers
         load_lib
-        
+
         initialize_logger
       end
-      
+
       def initialize_logger
         return if !config.logger.nil?
 
@@ -60,19 +60,19 @@ module Raemon
         else
           config.logger = Logger.new(STDOUT)
         end
-        
+
         # Set the logger level
         config.logger.level = instance_eval("Logger::#{config.log_level.to_s.upcase}")
-        
+
         # TODO: format the logger
         # config.logger.formatter
       end
-      
+
       def load_environment
         environment_file = "#{RAEMON_ROOT}/config/environments/#{RAEMON_ENV}.rb"
         eval IO.read(environment_file), binding
       end
-      
+
       def load_initializers
         load_folder "#{RAEMON_ROOT}/config/initializers"
       end
@@ -82,40 +82,40 @@ module Raemon
         $LOAD_PATH.unshift libdir
         load_folder libdir
       end
-      
+
       def load_folder(path)
         Dir["#{path}/**/*.rb"].each { |file| require(file) }
       end
-      
+
       def server_name
         @server_name = config.name || 'Raemon'
       end
-      
+
       def server_name_key
         server_name.downcase.gsub(' ', '_')
       end
-      
+
       def pid_file
         "#{RAEMON_ROOT}/tmp/pids/#{server_name_key}.pid"
       end
-      
+
       def running?
         pid = File.read(pid_file).to_i rescue 0
         Process.kill(0, pid) if pid > 0
       rescue Errno::ESRCH
       end
     end
-    
+
     class Configuration
       ATTRIBUTES = [ :name, :detach, :num_workers, :worker_klass,
                      :log_level, :logger, :timeout, :memory_limit ]
 
       attr_accessor *ATTRIBUTES
-      
+
       def [](key)
         send key rescue nil
       end
     end
-    
+
   end
 end
