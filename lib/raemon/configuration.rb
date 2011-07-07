0e69f4d0817f650787534dcf1fce639eef838c96
James Herdman <james.herdman@me.com>
2011-07-07 10:49:42 -0400

    Better handling of settings

--- lib/raemon/configuration.rb
+++ lib/raemon/configuration.rb
@@ -7,10 +7,28 @@ module Raemon
     ATTRIBUTES = [ :name, :detach, :num_workers, :worker_klass,
                    :log_level, :logger, :timeout, :memory_limit ]
 
-    attr_accessor *ATTRIBUTES
+    attr_accessor :settings
+    @settings = {}
 
-    def [](key)
-      send key rescue nil
+    # Define a configuration option with a default.
+    #
+    # @example Define the option.
+    #   Config.option(:name, default: "Raemon")
+    #
+    # @param [Symbol] name The name of the configuration option
+    #
+    # @param [Hash] options Extras for the option
+    #
+    # @note Copied from Mongoid. Thank you!
+    #
+    # @private
+    def option(name, options = {})
+      define_method(name) do
+        settings.has_key?(name) ? settings[name] : options[:default]
+      end
+
+      define_method("#{name}=") { |value| settings[name] = value }
+      define_method("#{name}?") { !!send(name) }
     end
   end
 end
--- spec/raemon/configuration_spec.rb
+++ spec/raemon/configuration_spec.rb
@@ -1,16 +1,25 @@
 require 'spec_helper'
 
 describe Raemon::Configuration do
-  describe '#[]' do
-    let(:name) { 'Cheese Burgers' }
-
-    it 'retrieves the value of the attribute' do
-      described_class.name = name
-      described_class[:name].should == name
+  describe '#option' do
+    before(:all) do
+      described_class.option(:test_setting, :default => true)
     end
 
-    it 'returns nil for an invalid attribute name' do
-      described_class[:splat].should be_nil
+    it "creates a getter for an option" do
+      described_class.should respond_to(:test_setting)
+    end
+
+    it "creates a setter for the option" do
+      described_class.should respond_to(:test_setting=)
+    end
+
+    it "creates a conditional for the option" do
+      described_class.should respond_to(:test_setting?)
+    end
+
+    it "allows the setting of a default value" do
+      described_class.test_setting.should == true
     end
   end
 end
