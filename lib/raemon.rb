746daef46978f077d7c6098c9ba37828a7d28da1
James Herdman <james.herdman@me.com>
2011-07-07 13:03:03 -0400

    A few convenience methods at the namespace level

--- lib/raemon.rb
+++ lib/raemon.rb
@@ -8,4 +8,21 @@ module Raemon
   autoload :Worker,        'raemon/worker'
   autoload :Server,        'raemon/server'
   autoload :Util,          'raemon/util'
+
+  # @return [Logger] the logger used by this library
+  def self.logger
+    Configuration.logger
+  end
+
+  # @yield [Raemon::Configuration] a block that allows for convenient configuration
+  #
+  # @example
+  #   Raemon.config do |c|
+  #     c.server_name = 'My Server'
+  #   end
+  #
+  # @see Raemon::Configuration
+  def self.config
+    yield Configuration
+  end
 end
---
+++ spec/raemon_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Raemon do
+  its(:logger) { should == Raemon::Configuration.logger }
+
+  describe '.config' do
+    it 'yields the configuration object' do
+      described_class.config do |config|
+        config.should == Raemon::Configuration
+      end
+    end
+  end
+end
