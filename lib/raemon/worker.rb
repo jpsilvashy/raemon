43f31b0ec168fc8644d164337cb2d33f4a3f3342
James Herdman <james.herdman@nulayer.com>
2011-07-07 09:44:33 -0400

    Tidy up Worker

--- lib/raemon/worker.rb
+++ lib/raemon/worker.rb
@@ -1,13 +1,12 @@
 module Raemon
   module Worker
-    
     def self.included(base)
       base.send :include, InstanceMethods
     end
-    
+
     module InstanceMethods
       attr_reader :master, :logger, :id, :pid, :pulse
-      
+
       def initialize(master, id, pulse)
         @master = master
         @logger = master.logger
@@ -15,19 +14,18 @@ module Raemon
         @pid    = $$
         @pulse  = pulse
       end
-      
+
       def ==(other_id)
         @id == other_id
       end
-      
+
       def run
         raise "Abstract method"
       end
-      
+
       def heartbeat!
         master.worker_heartbeat!(self)
       end
     end
-        
   end
 end
